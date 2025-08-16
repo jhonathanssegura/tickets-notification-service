a081cabf8109d4d6dc4d3c2b0d6436d9dcc01be7
Jhonathan Segura <jhonathanssegura@gmail.com>
2025-08-16 11:20:36 -0500

    Adding config for AWS

---
+++ internal/awsconfig/config.go
@@ -0,0 +1,19 @@
+package awsconfig
+
+import (
+	"context"
+
+	"github.com/aws/aws-sdk-go-v2/aws"
+	"github.com/aws/aws-sdk-go-v2/config"
+)
+
+func LoadAWSConfig() (aws.Config, error) {
+	return config.LoadDefaultConfig(context.TODO(),
+		config.WithRegion("us-east-1"),
+		config.WithEndpointResolver(aws.EndpointResolverFunc(
+			func(service, region string) (aws.Endpoint, error) {
+				return aws.Endpoint{URL: "http://localhost:4566"}, nil
+			})),
+	)
+}
+
